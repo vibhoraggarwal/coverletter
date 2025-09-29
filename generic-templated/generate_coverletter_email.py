0cf5a7963233d4efb238071c988e8ae1451ce637
vibhoraggarwal <vibhorag24@gmail.com>
2025-09-29 10:23:46 +0000

    code: adding automatic email

--- generic-templated/generate_coverletter_email.py
+++ generic-templated/generate_coverletter_email.py
@@ -12,29 +12,34 @@ with open("application_data.json") as f:
 # 2. Setup Jinja2
 # -----------------------
 env = Environment(loader=FileSystemLoader("."))
-template = env.get_template("coverletter_template.tex.jinja")
+template_coverletter = env.get_template("coverletter_template.tex.jinja")
+template_email = env.get_template("email_template.txt.jinja")
 
 # -----------------------
 # 3. Render template
 # -----------------------
-output_tex = template.render(**data)
 recipient_institution = data.get("recipient_institution")
 # If the value itself may contain spaces, normalize that
 if recipient_institution is not None:
     recipient_institution = recipient_institution.lower().replace(" ", "_")
-output_filename = f"coverletter_vibhor_aggarwal_{recipient_institution}.tex"
+output_filename_coverletter = f"coverletter_vibhor_aggarwal_{recipient_institution}.tex"
+output_filename_email = f"email_vibhor_aggarwal_{recipient_institution}.txt"
 
-with open(output_filename, "w") as f:
-    f.write(output_tex)
+with open(output_filename_coverletter, "w") as f:
+    f.write(template_coverletter.render(**data))
 
-print(f"Generated {output_filename}!")
+print(f"Generated {output_filename_coverletter}!")
+
+with open(output_filename_email, "w") as f:
+    f.write(template_email.render(**data))
+print(f"Generated {output_filename_email}!")
 
 # -----------------------
 # 4. Run pdflatex
 # -----------------------
 try:
     subprocess.run(
-        ["pdflatex", "-shell-escape", output_filename],
+        ["pdflatex", "-shell-escape", output_filename_coverletter],
         check=True
     )
     print("PDF compiled successfully!")
